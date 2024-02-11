9c00f808ba9c9ff09fe988f5fbf85ccf410aa7f1
Blueify <136642460+officialblueify@users.noreply.github.com>
2024-02-11 16:03:56 -0700

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,7 +1,9 @@
 function redirect(url) {
    window.location.replace(url);
 }
+export { redirect };
 
 function setTexturePackURL(url) {
     localStorage.setItem("bloxd-texturePackUrl", url);
 }
+export { setTexturePackURL };
